71b6438a9a3d7d0acfac32ad819af5e21c1753dc
IntelligentBeaver <aman.211506@ncit.edu.np>
2024-12-14 03:36:43 +0545

    news Page skeleton

--- lib/pages/news_page.dart
+++ lib/pages/news_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:skeletonizer/skeletonizer.dart';
 
 class NewsPage extends StatefulWidget {
   const NewsPage({super.key});
@@ -10,8 +11,21 @@ class NewsPage extends StatefulWidget {
 class _NewsPageState extends State<NewsPage> {
   @override
   Widget build(BuildContext context) {
-    return const Center(
-      child: Text("News Page"),
+    return SingleChildScrollView(
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: [
+          // Generate cards manually in a Column
+          for (int i = 0; i < 10; i++)
+            Card(
+              margin: EdgeInsets.symmetric(horizontal: 16, vertical: 8),
+              child: ListTile(
+                title: Text('News Title $i'),
+                subtitle: Text('https://newslink.com/item-$i'),
+              ),
+            ),
+        ],
+      ),
     );
   }
 }
